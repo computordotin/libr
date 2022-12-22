16543b3acd2ab1388a1d916bf6843dbdc8c9a3b6
vryfoundation <vedantsahu2019@gmail.com>
2022-12-22 13:15:08 +0530

    Spring Boot Starter Project Commit

---
+++ LibR/src/main/java/com/libr/LibRApplication.java
@@ -0,0 +1,13 @@
+package com.libr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibRApplication.class, args);
+	}
+
+}
